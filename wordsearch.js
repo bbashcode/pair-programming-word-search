9dffa23bb6205a0461b941c7fb5fe9bd21c3547f
bbashcode <binbash.code@gmail.com>
2022-01-20 03:31:26 +0000

    completed the pair programming problem

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
 const wordSearch = (letters, word) => { 
+    let val = true;
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+        if (horizontalJoin.includes(word)) {
+            val = true;
+        } else {
+            val = false;
+        }
+        
 }
+return val;
+};
 
-module.exports = wordSearch
+
+module.exports = wordSearch;
+
+/*
+Pair Programming assignment = pair-programming-word-search
+By: Md Meer and Muhammad Akbar 
+time to complete = 45 mins 
+*/
